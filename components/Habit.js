2a334cc0fa21db63d09ff9ae703e53540e1c56ba
Felix Keller <felixkeller98@gmail.com>
2020-01-24 17:19:44 +0100

    add apollo hook to query data

--- components/Habit.js
+++ components/Habit.js
@@ -13,7 +13,7 @@ const Habit = ({ habit, index }) => {
   const dates = getLast5Days();
   return (
     <article>
-      <h3 style={{ borderColor: colors[index] }}>{habit}</h3>
+      <h3 style={{ borderColor: colors[index] }}>{habit.name}</h3>
       <div className="buttons">
         {dates.map(date => <Button key={date.getTime()} date={date} />)}
       </div>
